75ac5cfd7939474b29da92ba58aef382ab8e2a58
ilathem <isaiahlathem@gmail.com>
2025-01-14 12:05:37 -0600

    switch to tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-const path = require('path')
-
-export default {
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  server: {
-    port: 8080,
-    hot: true
-  }
-}
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
